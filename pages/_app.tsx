c55eaabfc8b1ec7d0ef2bb190285a982bad125c1
Jan Czekala <jan.czekala99@web.de>
2021-11-09 13:47:21 +0100

    fix: hot-reload jss changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,23 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material/styles'
 import theme from '../theme'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import createEmotionCache from '../lib/createEmotionCache'
 
-function MyApp({ Component, pageProps }: AppProps) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
 
