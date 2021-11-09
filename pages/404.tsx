a00d6ce57f0c9ccaac7d352cbdc44a4a8c20bac8
Jan Czekala <jan.czekala99@web.de>
2021-11-09 17:09:40 +0100

    feat: error pages

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+const Custom404 = () => {
+    return (
+        <div className={styles.container} style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
+            <h1>404 - Page not found</h1>
+        </div>
+    )
+}
+
+export default Custom404
---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+const Custom500 = () => {
+    return (
+        <div className={styles.container} style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
+            <h1>Error code 500 occured</h1>
+        </div>
+    )
+}
+
+export default Custom500
